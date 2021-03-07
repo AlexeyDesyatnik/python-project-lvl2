5c858924a2a97bda1b90260f9d08191dee0b9bcd
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-03-07 20:22:21 +0300

    Basic argument parsing with argparse

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,11 @@
+import argparse
+
+
 def main():
-    print("Hello gendiff")
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
