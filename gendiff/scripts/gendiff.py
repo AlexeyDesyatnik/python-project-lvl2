e54748048b05116904667cb26e250b51d34b8ac5
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-03-08 22:15:49 +0300

    gendiff package now exports generate_diff() (stub for now)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,12 @@
 import argparse
+import json
+
+
+def generate_diff(fname1, fname2):
+    data1 = json.load(open(fname1))
+    data2 = json.load(open(fname2))
+    print(data1)
+    print(data2)
 
 
 def main():
@@ -7,6 +15,8 @@ def main():
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', type=str, help='set format of output')
     args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == "__main__":
