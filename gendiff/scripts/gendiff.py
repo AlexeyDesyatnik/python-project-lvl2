41c7310bc10ab7959579d6583173b4bfa8e8b54c
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-03-08 22:35:43 +0300

    Stage 3 requirements fulfilled

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,11 +2,34 @@ import argparse
 import json
 
 
+def format_key(name, value, state_char=' ', indent=2):
+    return "{}{} {}: {}".format(
+        ' ' * indent,
+        state_char,
+        name,
+        value)
+
+
 def generate_diff(fname1, fname2):
     data1 = json.load(open(fname1))
     data2 = json.load(open(fname2))
-    print(data1)
-    print(data2)
+    all_keys = data1.keys() | data2.keys()
+    removed_keys = data1.keys() - data2.keys()
+    added_keys = data2.keys() - data1.keys()
+    output = ['{']
+    for name in sorted(all_keys):
+        if name in removed_keys:
+            output.append(format_key(name, data1[name], state_char='-'))
+        elif name in added_keys:
+            output.append(format_key(name, data2[name], state_char='+'))
+        else:
+            if data1[name] == data2[name]:
+                output.append(format_key(name, data1[name], state_char=' '))
+            else:
+                output.append(format_key(name, data1[name], state_char='-'))
+                output.append(format_key(name, data2[name], state_char='+'))
+    output.append('}')
+    return '\n'.join(output)
 
 
 def main():
