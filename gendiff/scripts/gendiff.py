a456b41d6e9456e56278db111e92cfce7c800e7d
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-03-09 17:18:45 +0300

    Separated report generation and IO

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,26 +10,35 @@ def format_key(name, value, state_char=' ', indent=2):
         value)
 
 
-def generate_diff(fname1, fname2):
-    data1 = json.load(open(fname1))
-    data2 = json.load(open(fname2))
-    all_keys = data1.keys() | data2.keys()
-    removed_keys = data1.keys() - data2.keys()
-    added_keys = data2.keys() - data1.keys()
-    output = ['{']
+def report_dictionary_diff(dict1, dict2):
+    all_keys = dict1.keys() | dict2.keys()
+    removed_keys = dict1.keys() - dict2.keys()
+    added_keys = dict2.keys() - dict1.keys()
+    report = ['{']
     for name in sorted(all_keys):
+        line2 = None
         if name in removed_keys:
-            output.append(format_key(name, data1[name], state_char='-'))
+            line1 = format_key(name, dict1[name], state_char='-')
         elif name in added_keys:
-            output.append(format_key(name, data2[name], state_char='+'))
+            line1 = format_key(name, dict2[name], state_char='+')
         else:
-            if data1[name] == data2[name]:
-                output.append(format_key(name, data1[name], state_char=' '))
+            if dict1[name] == dict2[name]:
+                line1 = format_key(name, dict1[name], state_char=' ')
             else:
-                output.append(format_key(name, data1[name], state_char='-'))
-                output.append(format_key(name, data2[name], state_char='+'))
-    output.append('}')
-    return '\n'.join(output)
+                line1 = format_key(name, dict1[name], state_char='-')
+                line2 = format_key(name, dict2[name], state_char='+')
+        report.append(line1)
+        if line2:
+            report.append(line2)
+    report.append('}')
+    return report
+
+
+def generate_diff(fname1, fname2):
+    dict1 = json.load(open(fname1))
+    dict2 = json.load(open(fname2))
+    report = report_dictionary_diff(dict1, dict2)
+    return '\n'.join(report)
 
 
 def main():
